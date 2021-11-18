98dd758f585bc64cf2000077887ad8acb4656896
Gabrielgjs <gjsgabriel83@gmail.com>
2021-11-17 21:25:59 -0300

    alterações

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -1,11 +1,13 @@
 import { Tema } from './Tema';
 import { User } from './User';
-
+//PRODUTO
 export class Postagem{
   public id: number
-  public titulo: string
-  public texto: string
-  public data: Date
+  public curso: string
+  public descricao: string
+  public preco: number
+  public imagem: string
   public usuario: User
+  //CATEGORIA
   public tema: Tema
 }
