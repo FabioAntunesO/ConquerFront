06af5541a4e9345070e994438ebf7fa8c2b07bb2
matlencar <malencar292@gmail.com>
2021-11-18 14:41:25 -0300

    implementando cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.compon
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { UserEditComponent } from './edit/user-edit/user-edit.component';
 import { AlertasComponent } from './alertas/alertas.component';
+import { CardsComponent } from './cards/cards.component';
 
 
 
@@ -38,7 +39,8 @@ import { AlertasComponent } from './alertas/alertas.component';
     PostagemEditComponent,
     PostagemDeleteComponent,
     UserEditComponent,
-    AlertasComponent
+    AlertasComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
